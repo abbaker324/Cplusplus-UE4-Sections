8082e3d952cac2dabb6aa46709d8f7da1b89a7ad
Andrew Baker <sibermonkey@gmail.com>
2016-04-10 19:14:39 -0600

    BE71 Created new default pawn, overriding the default pawn provided by engine. Created grabber component.

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,52 @@
+// Copyright Andrew Baker 2016
+
+#include "BuildingEscape.h"
+#include "Grabber.h"
+
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// Get player view point this tick.
+	FVector Location;
+	FRotator Rotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT Location, OUT Rotation
+	);
+
+	UE_LOG(LogTemp, Warning, TEXT("Player view point information: [Locaton: %s] [Rotation: %s]"),
+		*Location.ToString(), *Rotation.ToString()
+	);
+
+	//Raycast out to reach distance.
+
+	//See what we hit.
+}
+
